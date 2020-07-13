c337111c8323c801d21cb1054b6d1284fd18bd05
lokesh53941 <65780262+lokesh53941@users.noreply.github.com>
2020-07-13 15:47:02 +0530

    initial commit1

--- copyOfFile.py
+++ copyOfFile.py
@@ -1,8 +1,22 @@
+import os
 import shutil
-
-original =input("enter orginal path")
-
-target = input("enter target path")
-
-shutil.copyfile(original, target)
+scr=input("enter path: ")
+#a=os.path.dirname(scr)
+#fileName=os.path.basename(scr)
+#print(fileName)
+base = os.path.splitext(scr)[1]
+if base==".py":
+    ext = os.path.splitext(scr)[0]
+    dst=str(ext) + str('- Copy.py')
+elif base==".pdf":
+    ext = os.path.splitext(scr)[0]
+    dst=str(ext) + str('- Copy.pdf')
+elif base==".txt":
+    ext = os.path.splitext(scr)[0]
+    dst=str(ext) + str('- Copy.txt')
+else:
+    ext = os.path.splitext(scr)[0]
+    dst=str(ext) + str('- Copy')
+print(dst)
+shutil.copy(scr,dst)
 
